91552adcf398a2153da09148ba91aaf31dda55d5
Web <web@it.step>
2021-08-07 11:57:19 +0300

    implemented treasure coords generation

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,12 @@ const hint = document.getElementById(`hint`);
 
 const map = document.querySelector(`[alt="map"]`);
 
+const ganerateRandomNumber = (size, gap) => Math.floor( Math.random() * (size - (gap*2))) + gap
+
+const getNewTreasureCoords = ({width,heigth}, gap) => ({
+    
+        x: generateRandomNumber(width, gap),
+        y: generateRandomNumber(heigth, gap)
+    
+});
+
