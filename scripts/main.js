d46703f09e7a9eea95774ba0414f7fb52b1f2779
Web <web@it.step>
2021-08-12 20:15:55 +0300

    function

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,8 @@ const hint = document.getElementById(`hint`);
 
 const map = document.querySelector(`[alt="map"]`);
 
+function getDistanceToTreasure({x: cX, y: cY}, {x: tX, y: tY}) {
+    return Math.sqrt( (cX - tX)**2 + (cY - tY)**2)
+};
+
+// console.log(getDistanceToTreasure({x: 2, y: 35},{x: 51 , y: 234}));
